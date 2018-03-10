a721d2f522ca97e7d36157a094b08e7b02bf5e65
Ron <ron@merehuman.net>
2018-03-10 13:27:51 -0600

    Added termIndex and proxVector to Processing
    Added termIndex and proxVector functions to processing to py
    Updated proceesingOuput reflects udpate - coincides with updated ingestTemplate.py output, using Quiz data to check for correctness

--- CODE/processing.py
+++ CODE/processing.py
@@ -3,22 +3,70 @@ from math import log10, sqrt    ## Doing a little math...
 import time                     ## For Timing
 from functools import wraps     ## For Timing
 
+inFile = 'OUTPUT/ingestOutput'
+outFile = 'OUTPUT/processingOutput'
+
 ##-------------------------------------------------------------------------##
+## INPUT
 ## Processing Component of Search Engine Space (follows ingest - preceeds
 ## Query execution
 ##-------------------------------------------------------------------------##
-## Expects two data structures from an ingest file, a term index and
-## document key:
-##  index = [ 
-##      { term: [tfs array] },
-##      { '/A-Za-z0-9/*': [ /0-9/*, ..., /0-9/* ] },
+## index = [
+##       { Term1: [tf1, tf2, ..., tfn] },
+##       { Term2: [tf1, tf2, ..., tfn] },
+##       ...,
+##       { Termm: [tf1, tf2, ..., tfn] }
+## ]
+## doc_key = [
+##       { DocName1: [DocID1, DocLocation1] },
+##       { DocName2: [DocID2, DocLocation2] },
+##       ...,
+##       { DocNamen: [DocIDn, DocLocationn] }
+## ]
+## proximity = {
+##       Term1: [ [DocID, Prox], [DocID, Prox], ..., [DocID, Prox] ],
+##       Term2: [ [DocID, Prox], [DocID, Prox], ..., [DocID, Prox] ],
+##       ...,
+##       Termm: [ [DocID, Prox], [DocID, Prox], ..., [DocID, Prox] ]
+## }
+##-------------------------------------------------------------------------##
+## OUTPUT
+##-------------------------------------------------------------------------##
+## doc_key = [
+##      { DocName1: [DocID1, DocLocation1] },
+##      { DocName2: [DocID2, DocLocation2] },
 ##      ...,
-##      { '/A-Za-z0-9/*': [ /0-9/*, ..., /0-9/* ] }
-##  doc_key = [
-##      { docName: [id, path] },
-##      { '/A-Za-z0-9/*': [/A-Za-z0-9/*, /A-Za-z0-9/* },
+##      { DocNamen: [DocIDn, DocLocationn] }
+## ]
+## docVector = [
+##      [WT1,D1, WT1,D2, ..., WT1,Dn],
+##      [WT2,D1, WT2,D2, ..., WT2,Dn],
 ##      ...,
-##      { '/A-Za-z0-9/*': [/A-Za-z0-9/*, /A-Za-z0-9/* }
+##      [WTm,D1, WTm,D2, ..., WTm,Dn]
+## ]
+## proxVector = [
+##      [ [P1T1,D1, P2T1,D1, ..., PiT1D1], 
+##        [P1T1,D2, P2T1,D2, ..., PiT1D2],
+##        ..., 
+##        [P1T1,Dn, P2T1,Dn, ..., PiT1Dn] ],
+##      [ [P1T2,D1, P2T2,D1, ..., PiT2D1], 
+##        [P1T2,D2, P2T2,D2, ..., PiT2D2], 
+##        ..., 
+##        [P1T2,Dn, P2T2,Dn, ..., PiT2Dn] ],
+##      ...,
+##      [ [P1Tm,D1, P2Tm,D1, ..., PiTmD1], 
+##        [P1Tm,D2, P2Tm,D2, ..., PiTmD2], 
+##        ..., 
+##        [P1Tm,Dn, P2Tm,Dn, ..., PiTmDn] ]
+## ] 
+## termIndex = {
+##      Term1: i1,
+##      Term2: i2,
+##      ...,
+##      Termm: im
+## }
+##-------------------------------------------------------------------------##
+
 ##-------------------------------------------------------------------------##
 
 ##-------------------------------------------------------------------------##
@@ -49,9 +97,10 @@ class genVSMArray:
                 ingest = shelve.open(inFile)
                 self.index = ingest['index']
                 self.doc_key = ingest['doc_key']
+                self.proximity = ingest['proximity']
                 ingest.close()
 
-                ## Sort the index
+                ## Sort the index and proximity (need to match)
                 self.index.sort(key=lambda k: k.keys()[0])
 
                 ## Initialize docLength Array (for normalizing weights)
@@ -59,8 +108,22 @@ class genVSMArray:
                 self.docLength = [0]*len(self.index[0][w])
                 
                 ## Initialize docVector Array (for storing VSM)
-                self.docVector = [ [0]*len(self.index) for _ in range(len(self.doc_key)) ]
-            
+                self.docVector = [ [0]*len(self.index) for _ 
+                        in range(len(self.doc_key)) ]
+
+                ## Restructure proximity and sort
+                self.prox = []
+                for x in self.proximity:
+                        self.prox.append( { x: self.proximity[x] } )
+                self.prox.sort(key = lambda k: k.keys()[0])
+
+                ## Initialize proxVector Array (for storing proximity)
+                self.proxVector = [ []*len(self.index) for _
+                        in range(len(self.doc_key)) ]
+
+                ## Initialize the termIndex
+                self.termIndex = {}
+
         ## Create tf-idf weight
         #@timing ## Uncomment to see discrete timing
         def genTFIDF(self):
@@ -111,6 +174,28 @@ class genVSMArray:
                                 self.docVector[x][y] = float("{0:.3f}".format(
                                         self.docVector[x][y]))
 
+        ## Create the proximity and term index
+        def genProx(self):
+                ## Go through each document
+                for d in range(len(self.doc_key)):
+                        doc = self.doc_key[d].keys()[0]
+                        ## Go through each term
+                        for t in range(len(self.prox)):
+                                term = self.prox[t].keys()[0]
+                                ## Initialize term array
+                                self.proxVector[d].append([])
+
+                                ## Build term index if first pass
+                                if d < 1:
+                                        self.termIndex[term] = t
+
+                                ## Go through each tuple
+                                for tuple in self.prox[t][term]:
+                                        ## if tuple[0] = d, then append prox
+                                        ## for this word
+                                        if tuple[0] == doc:
+                                                self.proxVector[d][t].append(tuple[1])
+
         #@timing ## Uncomment to see discrete timing
         def writeOutput(self, outFile):
                 ## Write the Document Vector Space Model (docVector) and
@@ -118,6 +203,8 @@ class genVSMArray:
                 out = shelve.open(outFile)
                 out['docVector'] = self.docVector
                 out['doc_key'] = self.doc_key
+                out['proxVector'] = self.proxVector
+                out['termIndex'] = self.termIndex
                 out.close()
 
 ##-----------------------------------------------------------------##
@@ -126,10 +213,11 @@ class genVSMArray:
 @timing
 def main():
         ## Execution as an object - result is VSM array
-        simpleVSM = genVSMArray('OUTPUT/ingestOutput')
+        simpleVSM = genVSMArray(inFile)
         simpleVSM.genTFIDF()
         simpleVSM.normalizeVectors()
-        simpleVSM.writeOutput('OUTPUT/processingOutput')
+        simpleVSM.genProx()
+        simpleVSM.writeOutput(outFile)
 
 ##-----------------------------------------------------------------##
 ## Execute the main... pretending to be C :)
