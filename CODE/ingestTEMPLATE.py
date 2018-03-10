be5e8be73360f6f19ece98b344fcf860a3a0529c
Ron <ron@merehuman.net>
2018-03-10 12:09:40 -0600

    Formatting, seeShelve, and Template
    Updated some formatting and text in the Progress Report
    Added seeShelve.py for easy view of the database (shelve) files - python seeShelve.py (or make it executable and run ./seeShelve.py)
    Updated ingestTempate.py to include proximity (from report format) - ingestOutput.db reflects the updated data structures

--- CODE/ingestTEMPLATE.py
+++ CODE/ingestTEMPLATE.py
@@ -1,18 +1,4 @@
 import shelve
-
-#
-# Proximity
-# { term: [ [ DocID, Prox ], [Doc ID, Prox], ... ],
-#   term: [ [ DocID, Prox ], [Doc ID, Prox], ... ],
-#   ...
-# }
-
-# Process:
-#              Term1               Term2
-# [ [ DocID [ [Prox, Prox, ... ], [Prox, Prox, ... ],
-
-
-
 #
 # From Quizes
 #       D1: Shipment of gold damaged in fire.
@@ -20,19 +6,12 @@ import shelve
 #       D3: Shipment of gold arrived in a truck.
 #       D4: Truck arrived damaged.
 
-#index = [
-#        { "term": "a", "tfs": [1,1,1,0] },
-#        { "term": "damaged", "tfs": [1,0,0,1] },
-#        { "term": "delivery", "tfs": [0,1,0,0] },
-#        { "term": "fire", "tfs": [1,0,0,0] },
-#        { "term": "arrived", "tfs": [0,1,1,1] },
-#        { "term": "gold", "tfs": [1,0,1,0] },
-#        { "term": "in", "tfs": [1,1,1,0] },
-#        { "term": "of", "tfs": [1,1,1,0] },
-#        { "term": "shipment", "tfs": [1,0,1,0] },
-#        { "term": "silver", "tfs": [0,2,0,0] },
-#        { "term": "truck", "tfs": [0,1,1,1] }
-#]
+# index = [
+#       { Term1: [tf1, tf2, ..., tfn] },
+#       { Term2: [tf1, tf2, ..., tfn] },
+#       ...,
+#       { Termm: [tf1, tf2, ..., tfn] }
+# ]
 index = [
         { "a": [1,1,1,0] },
         { "damaged": [1,0,0,1] },
@@ -47,12 +26,12 @@ index = [
         { "truck": [0,1,1,1] }
 ]
 
-#doc_key = [
-#        { "name": "D1", "path":"loc1" },
-#        { "name": "D2", "path":"loc2" },
-#        { "name": "D3", "path":"loc3" },
-#        { "name": "D4", "path":"loc4" }
-#]
+# doc_key = [
+#       { DocName1: [DocID1, DocLocation1] },
+#       { DocName2: [DocID2, DocLocation2] },
+#       ...,
+#       { DocNamen: [DocIDn, DocLocationn] }
+# ]
 doc_key = [
         { "D1": ["id1", "loc1"] },
         { "D2": ["id2", "loc2"] },
@@ -60,8 +39,28 @@ doc_key = [
         { "D4": ["id4", "loc4"] }
 ]
 
+# proximity = {
+#       Term1: [ [DocID, Prox], [DocID, Prox], ..., [DocID, Prox] ],
+#       Term2: [ [DocID, Prox], [DocID, Prox], ..., [DocID, Prox] ],
+#       ...,
+#       Termm: [ [DocID, Prox], [DocID, Prox], ..., [DocID, Prox] ]
+# }
+proximity = {
+        "a": [ ["D2", 6], ["D3", 6] ],
+        "damaged": [ ["D1", 4], ["D4", 3] ],
+        "delivery": [ ["D2", 1] ],
+        "fire": [ ["D1", 6] ],
+        "arrived": [ ["D2", 4], ["D3", 4], ["D4", 2] ],
+        "gold": [ ["D1", 3], ["D3", 3] ],
+        "in": [ ["D1", 5], ["D2", 5], ["D3", 5] ],
+        "of": [ ["D1", 2], ["D2", 2], ["D3", 2] ],
+        "shipment": [ ["D1", 1], ["D3", 1] ],
+        "silver": [ ["D2", 3], ["D2", 6] ],
+        "truck": [ ["D2", 8], ["D3", 7], ["D4", 1] ]
+}
+
 d = shelve.open('OUTPUT/ingestOutput')
 d['index'] = index
 d['doc_key'] = doc_key
+d['proximity'] = proximity
 d.close()
-
